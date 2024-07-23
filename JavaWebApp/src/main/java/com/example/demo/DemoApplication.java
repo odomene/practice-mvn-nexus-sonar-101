b18b8c7c6ed650a49f585bb33d34c5b91c527e57
Akani Odomene <odomene@yahoo.com>
2024-07-23 15:17:02 +0100

    Testing Sonarqube

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String DBPassword = "Admin@9238923";
+
+		system.out.println ("Hello");
+		system.out.println ("World");
+
 	}
 
 }
